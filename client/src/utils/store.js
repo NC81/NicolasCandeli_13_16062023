9b7a0f4ab04516dcdbf1e1b52fd44d4810c7d026
NC <nicolas.candeli@cegetel.net>
2023-08-23 16:46:52 +0200

    feat: add state cleaning on logout

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,21 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import profileReducer from '../features/user'
 import errorReducer from '../features/error'
 import toolsReducer from '../features/tools'
 
-const removeStorageMiddleware = (store) => (next) => (action) => {
+const combinedReducer = combineReducers({
+  user: profileReducer,
+  error: errorReducer,
+  tools: toolsReducer,
+})
+
+const rootReducer = (state, action) => {
   if (action.type === 'user/userLogout') {
+    state = undefined
     localStorage.removeItem('ArgentBank-token')
+    sessionStorage.removeItem('ArgentBank-token')
   }
-  return next(action)
+  return combinedReducer(state, action)
 }
 
 export const store = configureStore({
-  reducer: {
-    user: profileReducer,
-    error: errorReducer,
-    tools: toolsReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(removeStorageMiddleware),
+  reducer: rootReducer,
 })
